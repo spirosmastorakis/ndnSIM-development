7f149499a50a789b395de37d906f4c8408711f74
Spyridon Mastorakis <spiros.mastorakis@gmail.com>
2014-11-17 15:38:26 -0800

    ndn-ns3: Resolve conflicts manually.

--- model/ndn-ns3.hpp
+++ model/ndn-ns3.hpp
@@ -2,19 +2,12 @@
 #define NDN_NS3_HPP
 
 #include "ns3/packet.h"
-<<<<<<< HEAD
+#include "ns3/ptr.h"
 
 #include "ndn-header.hpp"
 #include "ndn-trailer.hpp"
 
-#include <ndn-cxx/encoding/block.hpp>
-#include <ndn-cxx/interest.hpp>
-#include <ndn-cxx/data.hpp>
-=======
-#include "ns3/ptr.h"
-
 #include <ndn-cxx/common.hpp>
->>>>>>> 46366be3d265891e64d92f874c23b11f572a8db7
 
 namespace ndn {
 
@@ -22,17 +15,9 @@ class Block;
 
 class UnknownHeaderException {};
 
-<<<<<<< HEAD
-class Convert
-{
-
-public:
-
-=======
 class Convert {
 
 public:
->>>>>>> 46366be3d265891e64d92f874c23b11f572a8db7
   static void
   ToPacket(shared_ptr<Block> block, ns3::Ptr<ns3::Packet> packet);
 
