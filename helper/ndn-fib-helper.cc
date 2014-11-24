20abd0d90b452a055324817145bf5a1ef302b04a
Spyridon Mastorakis <spiros.mastorakis@gmail.com>
2014-11-23 21:43:31 -0800

    ndn-fib-helper: RemoveNextHop method was added.

--- helper/ndn-fib-helper.cc
+++ helper/ndn-fib-helper.cc
@@ -37,7 +37,7 @@ FibHelper::GenerateCommand (Interest& interest)
 void
 FibHelper::AddNextHop (ControlParameters parameters, Ptr<Node> node)
 {
-  NS_LOG_DEBUG ("Send Command initialized");
+  NS_LOG_DEBUG ("Add Next Hop command was initialized");
   Block encodedParameters(parameters.wireEncode());
 
   Name commandName("/localhost/nfd/fib");
@@ -52,14 +52,43 @@ FibHelper::AddNextHop (ControlParameters parameters, Ptr<Node> node)
   // CommandValidator validator;
   // validator.addSupportedPrivilege("fib");
   // validator.addInterestRule(commandName.toUri(), key, *keyChain.getPublicKey (key));
-  //GenerateCommand(*command);
-  NS_LOG_DEBUG ("Command was generated");
+  // GenerateCommand(*command);
+  // NS_LOG_DEBUG ("Command was generated");
   // m_face->onReceiveData += [this, command, encodedParameters] (const Data& response) {
   //   NS_LOG_DEBUG ("NFD responded with " << response.getName());
   // };
   Ptr<L3Protocol> L3protocol = node->GetObject<L3Protocol> ();
   shared_ptr<FibManager> fibManager = L3protocol->GetFibManager ();
-  //fibManager->addInterestRule(commandName.toUri(), key, *keyChain.getPublicKey (key));
+  // fibManager->addInterestRule(commandName.toUri(), key, *keyChain.getPublicKey (key));
+  fibManager->onFibRequest(*command);
+}
+
+void
+FibHelper::RemoveNextHop (ControlParameters parameters, Ptr<Node> node)
+{
+  NS_LOG_DEBUG ("Remove Next Hop command was initialized");
+  Block encodedParameters(parameters.wireEncode());
+
+  Name commandName("/localhost/nfd/fib");
+  commandName.append("remove-nexthop");
+  commandName.append(encodedParameters);
+
+  shared_ptr<Interest> command(make_shared<Interest>(commandName));
+  KeyChain keyChain;
+  Name key;
+  key=keyChain.generateRsaKeyPairAsDefault("remove-nexthop");
+  keyChain.sign(*command);
+  // CommandValidator validator;
+  // validator.addSupportedPrivilege("fib");
+  // validator.addInterestRule(commandName.toUri(), key, *keyChain.getPublicKey (key));
+  // GenerateCommand(*command);
+  // NS_LOG_DEBUG ("Command was generated");
+  // m_face->onReceiveData += [this, command, encodedParameters] (const Data& response) {
+  //   NS_LOG_DEBUG ("NFD responded with " << response.getName());
+  // };
+  Ptr<L3Protocol> L3protocol = node->GetObject<L3Protocol> ();
+  shared_ptr<FibManager> fibManager = L3protocol->GetFibManager ();
+  // fibManager->addInterestRule(commandName.toUri(), key, *keyChain.getPublicKey (key));
   fibManager->onFibRequest(*command);
 }
 
